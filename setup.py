3bea9a9412b88e9d6b1a37ce372444ac3d097304
SergeM <sbmatyunin@gmail.com>
2020-12-17 00:21:35 +0100

    removed py 3.5 from setup.py

--- setup.py
+++ setup.py
@@ -19,14 +19,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Sergey Matyunin",
     author_email='serge-m@users.noreply.github.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
