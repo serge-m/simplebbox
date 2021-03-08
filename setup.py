8833142b50f2694225a3f5498485ac6fa4a6a3d4
SergeM <sbmatyunin@gmail.com>
2021-03-08 10:18:24 +0100

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/serge-m/simplebbox',
-    version='0.0.9',
+    version='0.0.10',
     zip_safe=False,
 )
